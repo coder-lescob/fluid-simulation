a89e7237f1c39023bfe53a991d8846d60ddc64a5
coder-lescob <gabriel.lescob@outlook.com>
2026-04-12 18:50:34 +0200

    hello, window

--- makefile
+++ makefile
@@ -2,7 +2,7 @@ CC := gcc
 
 DEBUG_FLAGS 	:= -g -Wall -Wextra -Wpedantic
 RELEASE_FLAGS 	:= 
-LD_FLAGS 		:= 
+LD_FLAGS 		:= $$(pkg-config --libs glfw3) $$(pkg-config --libs gl)
 
 BUILD_DIR 	:= build
 SRC_DIR		:= src
@@ -13,7 +13,7 @@ TARGET := $(BUILD_DIR)/fluid-simulation
 .PHONY: build
 build:
 	@mkdir -p $(BUILD_DIR)
-	@$(CC) $(DEBUG_FLAGS) $(SRC) -o $(TARGET) $(LD_FLAGS)
+	@$(CC) $(DEBUG_FLAGS) $(SRC) -o $(TARGET) $(LD_FLAGS) 
 
 run: build
 	@$(TARGET) $(ARGS)
--- src/main.c
+++ src/main.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <GLFW/glfw3.h>
+
+#define GLFW_OBJ_CHECK(OBJ) \
+    if (OBJ == NULL) {      \
+        glfwTerminate();    \
+        exit(-1);           \
+    }
 
 int main(void) {
-    printf("Hello, world !\n");
+
+    if (!glfwInit()) {
+        return -1;
+    }
+    
+    // creates a window
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Hello, world !", NULL, NULL);
+    GLFW_OBJ_CHECK(window);
+
+    // makes the window's context the current one
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        
+        // rendering
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // swap the buffers
+        glfwSwapBuffers(window);
+
+        // poll and process events
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
