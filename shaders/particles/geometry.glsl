cf478ab505af3b211c212c2dba5da35f937a315c
coder-lescob <gabriel.lescob@outlook.com>
2026-04-14 19:33:15 +0200

    * changed the rendering code to a loop because it looks cleaner * it required to change from 330 core to version 420 core, tho it is fine

--- shaders/particles/geometry.glsl
+++ shaders/particles/geometry.glsl
@@ -1,6 +1,9 @@
-#version 330 core
+#version 420 core
 layout (points) in;
 layout (triangle_strip, max_vertices = 4) out;
+
+// the number of vertices in a square
+#define NUM_CORNERS 4
 
 uniform vec2 window_size;
 uniform float particle_size;
@@ -9,21 +12,17 @@ out vec2 relative_coords;
 void main() {
     vec2 relative_square_size = vec2(particle_size, particle_size) / window_size;
 
-    relative_coords = vec2(-1, 1);
-    gl_Position = gl_in[0].gl_Position + vec4(-relative_square_size.x,  relative_square_size.y, 0.0, 0.0); 
-    EmitVertex();
-    
-    relative_coords = vec2(-1, -1);
-    gl_Position = gl_in[0].gl_Position + vec4(-relative_square_size.x, -relative_square_size.y, 0.0, 0.0);
-    EmitVertex();
+    vec2 corners[NUM_CORNERS] = {
+        vec2(-1,  1),
+        vec2(-1, -1),
+        vec2( 1,  1),
+        vec2( 1, -1),
+    };
 
-    relative_coords = vec2(1, 1);
-    gl_Position = gl_in[0].gl_Position + vec4( relative_square_size.x,  relative_square_size.y, 0.0, 0.0);
-    EmitVertex();
-
-    relative_coords = vec2(1, -1);
-    gl_Position = gl_in[0].gl_Position + vec4( relative_square_size.x, -relative_square_size.y, 0.0, 0.0); 
-    EmitVertex();
-    
+    for (int i = 0; i < NUM_CORNERS; i++) {
+        relative_coords = corners[i];
+        gl_Position = gl_in[0].gl_Position + vec4(relative_coords * relative_square_size, 0.0, 0.0); 
+        EmitVertex();
+    }
     EndPrimitive();
 }
